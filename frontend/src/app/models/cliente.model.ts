b55aa6b2abfe0eeb72523bf9e90a254fff16857c
ENRIQUE PANADERO GIJON <6003801@viewnext.com>
2026-03-23 12:32:56 +0100

    Proyecto Angular y modelo Cliente

---
+++ frontend/src/app/models/cliente.model.ts
@@ -0,0 +1,10 @@
+
+// Estructura de un cliente
+export interface Cliente {
+    id?: number;
+    nombre: string;
+    apellidos: string;
+    telefono: string;
+    email: string;
+    direccion: string;
+}
